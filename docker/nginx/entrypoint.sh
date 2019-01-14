b035de4154b4088f1405b278f20ed32e8671b1e7
elgrim <alexandre.vagnair@hetic.net>
2019-01-14 12:12:13 +0100

    [DEV]

--- docker/nginx/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ln -s /etc/nginx/sites-available/symfony-${SYMFONY_ENV}.conf /etc/nginx/sites-enabled/symfony-${SYMFONY_ENV}.conf
-rm /etc/nginx/sites-enabled/default
-# Run nginx
-nginx

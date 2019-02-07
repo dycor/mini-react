7f909cd1521e6c971d1dd4a4011f01754ac064c8
Domange <backinsmail@gmail.com>
2019-02-07 12:51:43 +0100

    Create class Component with type_check and type_check v2

--- martine.js
+++ martine.js
@@ -73,3 +73,49 @@ export const MartineDOM = {
 };
 
 
+
+  class Component
+  {
+      type_check_v2(data, conf) {
+          for (let key of Object.keys(conf)) {
+              switch (key) {
+                  case 'type':
+                      if (!type_check_v1(data, conf[key])) return false;
+                      break;
+                  case 'value':
+                      if (JSON.stringify(data) !== JSON.stringify(conf[key])) return false;
+                      break;
+                  case 'enum':
+                      let valid = false;
+                      for (let value of conf[key]) {
+                          valid = type_check_v2(data, {value});
+                          if (valid) break;
+                      }
+                      if(!valid) return false;
+              }
+          }
+
+          return true;
+      }
+
+      type_check(data, conf) {
+          for (let key of Object.keys(conf)) {
+              switch (key) {
+                  case 'type':
+                  case 'value':
+                  case 'enum':
+                      let newConf = {};
+                      newConf[key] = conf[key];
+                      if (!this.type_check_v2(data, newConf)) return false;
+                      break;
+                  case 'properties':
+                      for (let prop of Object.keys(conf[key])) {
+                          if (data[prop] === undefined) return false;
+                          if (!type_check(data[prop], conf[key][prop])) return false;
+                      }
+                      break;
+              }
+          }
+          return true;
+      }
+  }

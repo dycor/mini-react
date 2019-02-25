e1f46aaef2dc46a553246b4b29991fc2f9be18ad
Domange <backinsmail@gmail.com>
2019-02-25 17:45:06 +0100

    En attente d'information sur props

--- martine.js
+++ martine.js
@@ -15,9 +15,6 @@ class Component {
     setArrayTypeCheck(array){
         this.arrayTypeCheck = array;
     }
-    setArrayValueCheck(array){
-        this.arrayValueCheck = array;
-    }
     propType(){
         console.log(type_check_v1({ "prop1": 1 }, "object"));
         for(let value in this.arrayTypeCheck) {
@@ -98,3 +95,70 @@ export const MartineDOM = {
     domElement.appendChild(currentDOM);
   }
 };
+
+
+
+function type_check_v1(data, type) {
+    switch(typeof data) {
+        case "number":
+        case "string":
+        case "boolean":
+        case "undefined":
+        case "function":
+            return type === typeof data;
+        case "object":
+            switch(type) {
+                case "null":
+                    return data === null;
+                case "array":
+                    return Array.isArray(data);
+                default:
+                    return data !== null && !Array.isArray(data);
+            }
+
+    }
+
+    return false;
+}
+function type_check_v2(data, conf) {
+    for (let key of Object.keys(conf)) {
+        switch (key) {
+            case 'type':
+                if (!type_check_v1(data, conf[key])) return false;
+                break;
+            case 'value':
+                if (JSON.stringify(data) !== JSON.stringify(conf[key])) return false;
+                break;
+            case 'enum':
+                let valid = false;
+                for (let value of conf[key]) {
+                    valid = type_check_v2(data, {value});
+                    if (valid) break;
+                }
+                if(!valid) return false;
+        }
+    }
+
+    return true;
+}
+function type_check(data, conf) {
+    for (let key of Object.keys(conf)) {
+        switch (key) {
+            case 'type':
+            case 'value':
+            case 'enum':
+                let newConf = {};
+                newConf[key] = conf[key];
+                if (!type_check_v2(data, newConf)) return false;
+                break;
+            case 'properties':
+                for (let prop of Object.keys(conf[key])) {
+                    if (data[prop] === undefined) return false;
+                    if (!type_check(data[prop], conf[key][prop])) return false;
+                }
+                break;
+        }
+    }
+
+    return true;
+}

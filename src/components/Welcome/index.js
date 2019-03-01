2f8e4a51fb193b9d51fe7f58e694673ed8cbf5ea
elgrim <alexandre.vagnair@hetic.net>
2019-03-01 01:24:46 +0100

    [DEV] add welcome page

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -3,6 +3,13 @@ import {Martine} from "../../../martine.js";
 export class Welcome extends Martine.Component  {
   render() {
      return  Martine.createElement('div',null,
-         Martine.createElement('h1', null, 'Welcome'));
+         Martine.createElement('h1', null, 'Welcome'),
+         Martine.createElement('div', null,
+            Martine.createElement('h2', null, 'Bonjour est bienvenue sur le micro framework Martine.js'),
+             Martine.createElement('div', null,
+                 Martine.createElement('p', null, 'Découvrir le framework Martine.js'),
+             )
+        )
+     );
   }
 }
